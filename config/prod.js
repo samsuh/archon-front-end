ce21241af78b7f6d0c8af1a46af054ceafff4bd2
Samuel Suh <samsuh@gmail.com>
2020-01-05 14:58:47 -0800

    updated heroku keys for jwt

--- config/prod.js
+++ config/prod.js
@@ -7,4 +7,5 @@ module.exports = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN,
+  jwtSecret: process.env.JWT_SECRET,
 };
